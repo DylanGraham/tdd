632b5e985eff6694bc3223179c9976865f35456b
Dylan Graham <dylan.graham@gmail.com>
2015-09-12 11:11:43 +1000

    gulp tdd

---
+++ tests/PracticeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class PracticeTest extends TestCase
+{ 
+    public function testStuff()
+    {
+        $names = ['Dylan', 'Taylor', 'Troll'];
+
+        $this->assertContains('Troll', $names, "TROLL!");
+    }
+}
