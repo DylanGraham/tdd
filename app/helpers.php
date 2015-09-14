b8e89cf949375610b9edc75325b5bd477b927bba
Dylan Graham <dylan.graham@gmail.com>
2015-09-14 22:35:58 +1000

    helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,10 @@
 <?php
 
-function link_me_to($url, $body)
+function link_me_to($url, $body, $parameters = null)
 {
     $url = url($url);
+
+    $attributes = $parameters ? Html::attributes($parameters) : '';
+
     return "<a href='{$url}'>{$body}</a>";
 }
